4571607e504054aecd4842f56807db49c1ecfbf3
Nils Hodys <dev@pmail.nils.bz>
2020-02-26 17:59:46 +0100

    [master] add more google chrome files

--- google-chrome.sh
+++ google-chrome.sh
@@ -3,3 +3,6 @@
 rm -rf ~/Library/Application\ Support/Google/Chrome
 rm -rf ~/Applications/Chrome\ Apps.localized
 rm -f ~/Library/Google/Google Chrome Brand.plist
+rm -rf ~/Library/Saved\ Application\ State/com.google.Chrome.savedState
+rm -f ~/Library/Preferences/com.google.Chrome.plist
+rm -rf ~/Library/Caches/Google/Chrome
