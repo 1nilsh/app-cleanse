e8f9939da8f06b566818fbbb94348386f10c5a16
Nils Hodys <dev@pmail.nils.bz>
2020-02-26 18:00:18 +0100

    [master] add opera script

---
+++ opera.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm -rf ~/Library/Caches/com.operasoftware.Opera
+rm -f ~/Library/Preferences/com.operasoftware.Opera.plist
+rm -rf ~/Library/Saved\ Application\ State/com.operasoftware.Opera.savedState
