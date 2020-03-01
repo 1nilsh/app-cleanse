90ebef6f407e914b9ccc23285d7e8fa5db8372f4
Nils Hodys <dev@pmail.nils.bz>
2020-03-01 14:01:45 +0100

    [master] add lastpass

---
+++ lastpass.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm -rf ~/Library/Application\ Support/com.lastpass.lastpassmacdesktop
+rm -rf ~/Library/Application\ Scripts/com.lastpass.lastpassmacdesktop.safariext
+rm -rf ~/Library/Caches/com.lastpass.lastpassmacdesktop
+rm -rf ~/Library/Containers/com.lastpass.LastPass
+rm -rf ~/Library/Containers/com.lastpass.lastpassmacdesktop.safariext
+rm -f ~/Library/Preferences/com.lastpass.lastpassmacdesktop.plist
+rm -rf ~/Library/WebKit/com.lastpass.lastpassmacdesktop 
