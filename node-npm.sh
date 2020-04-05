bfb210acf166d8eccc96a643ad064c1a968b19a4
Nils Hodys <dev@pmail.nils.bz>
2020-04-05 18:45:35 +0200

    [master] add node and npm

---
+++ node-npm.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm -rf /usr/local/lib/node_modules
+rm -rf /usr/local/include/node
+rm -rf /usr/local/bin/node
+rm -rf /usr/local/bin/npm
+rm -rf ~/.npm
+rm -rf /usr/local/share/man/man1/node*
+rm -rf /usr/local/share/man/man1/npm*
+rm -f /usr/local/lib/dtrace/node.d
+rm -rf /usr/local/share/doc/node
+rm -f /usr/local/share/systemtap/tapset/node.stp
